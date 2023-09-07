f30f931b92efd54a3301c7711d3c52b6cc204b1f
Ilef01 <bchinyyilef@gmail.com>
2023-09-07 13:52:10 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * This is the main fuction.
+ * It displays a messaage and returns 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

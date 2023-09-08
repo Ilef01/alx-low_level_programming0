64541871aabe1de0e0f28fa286d6c19bff768c64
Ilef01 <bchinyyilef@gmail.com>
2023-09-08 13:31:42 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This function prints numbers of base 10 starting from 0
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(char(i));
+	putchar('\n');
+	return (0);
+}
